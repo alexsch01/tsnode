13d3d2ff91f11518a36233853809a508ccc64c81
Alex Schwartz <alexschwartz01@gmail.com>
2025-06-27 06:45:18 -0400

    add `--init` command

--- bin.js
+++ bin.js
@@ -6,12 +6,50 @@ if (process.platform === 'win32') {
 } else {
     ts = require(require('path').resolve(__dirname, 'lib', 'node_modules', 'typescript'))
 }
+const { resolve } = require('path')
+const { existsSync, writeFileSync } = require('fs')
 const { spawn } = require('child_process')
 
 const myArgs = process.argv.slice(2)
+
+if (myArgs[0] === '--init') {
+    if (myArgs.length !== 1) {
+        console.error("Error: Unknown command")
+        process.exit(1)
+    }
+
+    const packageJSON = resolve(process.cwd(), 'package.json')
+    const tsconfigJSON = resolve(process.cwd(), 'tsconfig.json')
+
+    if (!existsSync(packageJSON)) {
+        writeFileSync(packageJSON, 
+            `{
+    "type": "module"
+}`
+        )
+    }
+    if (!existsSync(tsconfigJSON)) {
+        writeFileSync(tsconfigJSON,
+            `{
+    "compilerOptions": {
+        "noEmit": true,
+        "target": "esnext",
+        "module": "nodenext",
+        "rewriteRelativeImportExtensions": true,
+        "erasableSyntaxOnly": true,
+        "verbatimModuleSyntax": true
+    }
+}`
+        )
+    }
+
+    process.exit()
+}
+
 const firstTSFile = myArgs.find(a => a.endsWith('.ts'))
 if (firstTSFile === undefined) {
-    throw new Error("Must provide a TS file")
+    console.error("Error: Must provide a ts file")
+    process.exit(1)
 }
 
 const program = ts.createProgram([firstTSFile], {
