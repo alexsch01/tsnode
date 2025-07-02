05d208a587328c000ccd5ba1882c5c2649b30eca
Alex Schwartz <alexschwartz01@gmail.com>
2025-07-02 15:05:57 -0400

    last require->import

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,4 +1,4 @@
-const { execSync } = require('node:child_process')
-const { resolve } = require('node:path')
+import { execSync } from 'node:child_process'
+import { resolve } from 'node:path'
 
 execSync(`npm install @typescript/native-preview --prefix "${resolve(import.meta.dirname, '..')}" --global --no-bin-links`)
