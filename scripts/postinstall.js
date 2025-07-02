85e154ab3f03d8d4c60d9513a0a885d5747cfdff
Alex Schwartz <alexschwartz01@gmail.com>
2025-07-02 14:59:44 -0400

    Update postinstall.js

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,4 +1,4 @@
 const { execSync } = require('node:child_process')
 const { resolve } = require('node:path')
 
-execSync(`npm install @typescript/native-preview --prefix "${resolve(__dirname, '..')}" --global --no-bin-links`)
+execSync(`npm install @typescript/native-preview --prefix "${resolve(import.meta.dirname, '..')}" --global --no-bin-links`)
