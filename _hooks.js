ea8c00b9294c1d80e3802bd7139fb81529da38af
Alex Schwartz <alexschwartz01@gmail.com>
2025-08-06 14:01:58 -0400

    Create _hooks.js

---
+++ _hooks.js
@@ -0,0 +1,31 @@
+import { spawnSync } from 'node:child_process'
+import { resolve } from 'node:path'
+
+let mainScript
+
+export function load(url, context, nextLoad) {
+    mainScript ??= url
+
+    if (url === mainScript) {
+        let tscPath
+        if (process.platform === 'win32') {
+            tscPath = resolve(import.meta.dirname, 'node_modules', '@typescript', 'native-preview', 'bin', 'tsgo.js')
+        } else {
+            tscPath = resolve(import.meta.dirname, 'lib', 'node_modules', '@typescript', 'native-preview', 'bin', 'tsgo.js')
+        }
+
+        const { status: tscStatus, stdout: tscStdout } = spawnSync('node', [tscPath])
+
+        if (tscStatus === 1) {
+            console.error("Error: tsconfig.json not found, run tsnode --init")
+            process.exit(1)
+        }
+
+        if (tscStatus !== 0) {
+            process.stderr.write(tscStdout)
+            process.exit(1)
+        }
+    }
+
+    return nextLoad(url, context)
+}
