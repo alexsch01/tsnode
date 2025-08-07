6e31a3178428744eb506466f89a5cf86e60a438a
Alex Schwartz <alexschwartz01@gmail.com>
2025-08-07 14:16:10 -0400

    Update postinstall.js

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,5 +1,4 @@
 import { execSync } from 'node:child_process'
 import { resolve } from 'node:path'
 
-// see https://github.com/microsoft/typescript-go/issues/1493
-execSync(`npm install @typescript/native-preview@7.0.0-dev.20250723.1 --prefix "${resolve(import.meta.dirname, '..')}" --global --no-bin-links`)
+execSync(`npm install @typescript/native-preview --prefix "${resolve(import.meta.dirname, '..')}" --global --no-bin-links`)
