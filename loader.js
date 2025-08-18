eb914f41d1871d4bc555bf833bed8d115d204c43
Alex Schwartz <alexschwartz01@gmail.com>
2025-08-18 15:33:14 -0400

    allow NODE_OPTIONS env to work

--- loader.js
+++ loader.js
@@ -3,42 +3,44 @@ import { existsSync } from 'node:fs'
 import { spawnSync } from 'node:child_process'
 import { resolve } from 'node:path'
 
-const myArgs = process.argv
-const packageJSON = resolve(process.cwd(), 'package.json')
-const tsconfigJSON = resolve(process.cwd(), 'tsconfig.json')
-
-if ( myArgs.find(a => a.endsWith('.ts')) === undefined ) {
-    console.error("Error: must provide a ts file")
-    process.exit(1)
+let tscPath
+if (process.platform === 'win32') {
+    tscPath = resolve(import.meta.dirname, 'node_modules', '@typescript', 'native-preview', 'bin', 'tsgo.js')
+} else {
+    tscPath = resolve(import.meta.dirname, 'lib', 'node_modules', '@typescript', 'native-preview', 'bin', 'tsgo.js')
 }
 
-if (!existsSync(packageJSON) || !existsSync(tsconfigJSON)) {
-    console.error("Error: run npx tsnode --init")
-    process.exit(1)
+if (process.argv[1] !== tscPath) {
+    const myArgs = process.argv
+    const packageJSON = resolve(process.cwd(), 'package.json')
+    const tsconfigJSON = resolve(process.cwd(), 'tsconfig.json')
+    
+    if ( myArgs.find(a => a.endsWith('.ts')) === undefined ) {
+        console.error("Error: must provide a ts file")
+        process.exit(1)
+    }
+    
+    if (!existsSync(packageJSON) || !existsSync(tsconfigJSON)) {
+        console.error("Error: run npx tsnode --init")
+        process.exit(1)
+    }
+    
+    let firstScript = true
+    
+    registerHooks({
+        load(url, context, nextLoad) {
+            if (firstScript) {
+                const { status: tscStatus } = spawnSync('node', [tscPath], { stdio: ['inherit', process.stderr, 'inherit'] })
+                if (tscStatus !== 0) {
+                    const output = nextLoad(url, context)
+                    output.source = Buffer.from('process.exit(1)')
+                    return output
+                }
+    
+                firstScript = false
+            }
+    
+            return nextLoad(url, context)
+        },
+    })
 }
-
-let firstScript = true
-
-registerHooks({
-    load(url, context, nextLoad) {
-        if (firstScript) {
-            let tscPath
-            if (process.platform === 'win32') {
-                tscPath = resolve(import.meta.dirname, 'node_modules', '@typescript', 'native-preview', 'bin', 'tsgo.js')
-            } else {
-                tscPath = resolve(import.meta.dirname, 'lib', 'node_modules', '@typescript', 'native-preview', 'bin', 'tsgo.js')
-            }
-
-            const { status: tscStatus } = spawnSync('node', [tscPath], { stdio: ['inherit', process.stderr, 'inherit'] })
-            if (tscStatus !== 0) {
-                const output = nextLoad(url, context)
-                output.source = Buffer.from('process.exit(1)')
-                return output
-            }
-
-            firstScript = false
-        }
-
-        return nextLoad(url, context)
-    },
-})
