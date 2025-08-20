094e4a0c9343791bafb9418c8d076a02401b3606
Alex Schwartz <alexschwartz01@gmail.com>
2025-08-20 09:31:13 -0400

    Delete scripts directory

--- scripts/postinstall.js
+++
@@ -1,4 +0,0 @@
-import { execSync } from 'node:child_process'
-import { resolve } from 'node:path'
-
-execSync(`npm install @typescript/native-preview --prefix "${resolve(import.meta.dirname, '..')}" --global --no-bin-links`)
