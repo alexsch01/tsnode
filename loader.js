ddadcf1abd18b622c6ecc6de9b61dcda9dc41b85
Alex Schwartz <alexschwartz01@gmail.com>
2025-08-20 11:16:59 -0400

    simplify `--import=@alexsch01/tsnode` loader

--- loader.js
+++ loader.js
@@ -8,37 +8,39 @@ if (!existsSync(tscPath)) {
     tscPath = resolve(process.cwd(), 'node_modules', '@typescript', 'native-preview', 'bin', 'tsgo.js') // local install
 }
 
-if (process.argv[1] !== tscPath) {
-    const myArgs = process.argv
-    const packageJSON = resolve(process.cwd(), 'package.json')
-    const tsconfigJSON = resolve(process.cwd(), 'tsconfig.json')
-    
-    if ( myArgs.find(a => a.endsWith('.ts')) === undefined ) {
-        console.error("Error: must provide a ts file")
-        process.exit(1)
-    }
-    
-    if (!existsSync(packageJSON) || !existsSync(tsconfigJSON)) {
-        console.error("Error: run npx tsnode --init")
-        process.exit(1)
-    }
-    
-    let firstScript = true
-    
-    registerHooks({
-        load(url, context, nextLoad) {
-            if (firstScript) {
-                const { status: tscStatus } = spawnSync('node', [tscPath], { stdio: ['inherit', process.stderr, 'inherit'] })
-                if (tscStatus !== 0) {
-                    const output = nextLoad(url, context)
-                    output.source = Buffer.from('process.exit(1)')
-                    return output
-                }
-    
-                firstScript = false
-            }
-    
-            return nextLoad(url, context)
-        },
-    })
+const myArgs = process.argv
+const packageJSON = resolve(process.cwd(), 'package.json')
+const tsconfigJSON = resolve(process.cwd(), 'tsconfig.json')
+
+if ( myArgs.find(a => a.endsWith('.ts')) === undefined ) {
+    console.error("Error: must provide a ts file")
+    process.exit(1)
 }
+
+if (!existsSync(packageJSON) || !existsSync(tsconfigJSON)) {
+    console.error("Error: run npx tsnode --init")
+    process.exit(1)
+}
+
+let firstScript = true
+
+registerHooks({
+    load(url, context, nextLoad) {
+        if (firstScript) {
+            const { status: tscStatus } = spawnSync('node', [tscPath], {
+                stdio: ['inherit', process.stderr, 'inherit'],
+                env: { NODE_OPTIONS: '' },
+            })
+
+            if (tscStatus !== 0) {
+                const output = nextLoad(url, context)
+                output.source = Buffer.from('process.exit(1)')
+                return output
+            }
+
+            firstScript = false
+        }
+
+        return nextLoad(url, context)
+    },
+})
